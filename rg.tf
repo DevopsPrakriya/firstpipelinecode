f45ef0e986a8c73b29a28a8204e9f92b0634825f
swapnil060791 <68774032+swapnil060791@users.noreply.github.com>
2025-07-14 14:28:36 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,17 +2,3 @@ resource "azurerm_resource_group" "swapnil" {
   name     = "swapnilrevision"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "swapnil2" {
-  name     = "swapnilrevision2"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "swapnil3" {
-  name     = "swapnilrevision3"
-  location = "centralus"
-}
-
-resource "azurerm_resource_group" "swapnil4" {
-  name     = "swapnilrevision4"
-  location = "centralus"
-}
